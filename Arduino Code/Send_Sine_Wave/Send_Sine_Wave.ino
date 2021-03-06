7bccbc360d6eb9da0d965aa0f6c622e592a551e8
Alick Campbell <alickcampbell54321@gmail.com>
2021-03-06 02:10:28 -0500

    Edited configuration codes

--- Arduino Code/Send_Sine_Wave/Send_Sine_Wave.ino
+++ Arduino Code/Send_Sine_Wave/Send_Sine_Wave.ino
@@ -2,45 +2,16 @@
 Code writed to send hardcoded data to android device for testing
 */
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(8, 9); // RX | TX
+SoftwareSerial BTSerial(10, 11); // RX | TX
 #define nPoints 16
 #define rawnPoints 32
 unsigned char data[2 * nPoints] = {0,0,3,255,3,255,0,0,0,0,3,255,3,255,0,0,0,0,3,255,3,255,0,0,0,0,3,255,3,255,0,0};
 int samples[nPoints];
 
-// function to convert samples from ADC into an array of 8 bit chars. Every 2 byte represents a single value obtained from the ADC
-//void toUnsignedChar() {
-//  for (int i = 0; i < nPoints; i++) {
-//    data[2*i] = samples[i] >> 8;
-//    data[2*i+1] = samples[i] & 0b0000000011111111;
-//  }
-//}
-  
-  
-
-
 void setup()
 {
-  Serial.begin(9600);
-
- 
-// for (int i = 0 ; i < 32; i++)
-// Serial.println(data[i]);
-//  Serial.println();
-//    Serial.println();
-//      Serial.println();
-//  uint16_t arr[nPoints];
-//  uint16_t a; // Temporary holder
-//  //convert to 2 byte data
-//  for(int i = 0; i < nPoints; i++) {
-//      a = 0;
-//      a = data[2*i]<<8;
-//      arr[i] = a + data[2*i+1];
-//    Serial.println(arr[i]);
-//  }
-  
- 
-  BTSerial.begin(9600);  // HC-05 default speed in AT command more
+  Serial.begin(9600); 
+  BTSerial.begin(115200);  
 }
 
 void loop()
@@ -57,6 +28,36 @@ void loop()
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   //Populate array like ABBAABBAAB
--- Spectroscilloscope_1/app/src/main/java/com/example/spectroscilloscope/MainActivity.java
+++ Spectroscilloscope_1/app/src/main/java/com/example/spectroscilloscope/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -47,8 +48,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
         startButton = (Button) findViewById(R.id.buttonStart);
         sendButton = (Button) findViewById(R.id.buttonSend);
         clearButton = (Button) findViewById(R.id.buttonClear);
